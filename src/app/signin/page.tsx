1ab245627df534ba7616d7a035f1013f5577ea77
student1060632fpt <haveaniceday6174@gmail.com>
2024-03-06 13:09:24 +0700

    xong sign in

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import Image from "next/image";
+import Background from "./../../assets/images/bg-signin.jpg";
+import { Button } from "@mui/material";
+import Link from "next/link";
+import FormLogin from "@/components/auth/FormLogin";
+import { Metadata } from "next";
+import { SITE_NAME } from "@/utils/env";
+import "./../login/Login.scss"
+import FormSignin from "@/components/auth/FormSignin";
+
+export const metadata: Metadata = {
+  title: "Sign In | " + SITE_NAME,
+};
 
 const SignIn = () => {
   return (
-    <div>SignIn</div>
-  )
-}
+    <div className="auth">
+      <div className="auth__left ">
+        <h2 className="">
+          Đăng ký
+        </h2>
+        <div className="auth__form ">
+          <FormSignin />
 
-export default SignIn
+          <Button variant="contained" sx={{ color: "white" }} size="large">
+            Đăng ký
+          </Button>
+          <div className="flex">
+            <p className="text-gray-500">Có tài khoản rồi?</p>
+            <Link href="/login">
+              <p className="text-secondary mx-1">Đăng nhập</p>
+            </Link>
+            <p className="text-gray-500">nhé</p>
+          </div>
+        </div>
+      </div>
+      <div className="auth__right">
+        <Image
+          src={Background}
+          alt="background"
+          fill
+          className="img"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default SignIn;
--- src/components/auth/FormSignin.tsx
+++ src/components/auth/FormSignin.tsx
@@ -1,11 +1,21 @@
 "use client";
 
 import { Visibility, VisibilityOff } from "@mui/icons-material";
-import { Checkbox, FormControl, FormControlLabel, IconButton, Input, InputAdornment, InputLabel, TextField } from "@mui/material";
+import {
+  Checkbox,
+  FormControl,
+  FormControlLabel,
+  IconButton,
+  Input,
+  InputAdornment,
+  InputLabel,
+  TextField,
+} from "@mui/material";
 import React, { MouseEvent, useState } from "react";
 
-const FormLogin = () => {
+const FormSignin = () => {
   const [showPassword, setShowPassword] = useState(false);
+  const [showRepeatPassword, setShowRepeatPassword] = useState(false);
 
   const handleClickShowPassword = () => setShowPassword((show) => !show);
 
@@ -38,14 +48,32 @@ const FormLogin = () => {
           }
         />
       </FormControl>
+      <FormControl variant="standard" sx={{ width: "100%" }}>
+        <InputLabel htmlFor="standard-adornment-password">Nhập lại mật khẩu</InputLabel>
+        <Input
+          id="standard-adornment-password"
+          type={showRepeatPassword ? "text" : "password"}
+          endAdornment={
+            <InputAdornment position="end">
+              <IconButton
+                aria-label="toggle password visibility"
+                onClick={() => setShowRepeatPassword(state => !state)}
+                onMouseDown={handleMouseDownPassword}
+              >
+                {showRepeatPassword ? <VisibilityOff /> : <Visibility />}
+              </IconButton>
+            </InputAdornment>
+          }
+        />
+      </FormControl>
       <FormControl>
         <FormControlLabel
           control={<Checkbox defaultChecked />}
-          label="Nhớ mật khẩu"
+          label="Tôi đồng ý với điều khoản bảo mật"
         />
       </FormControl>
     </>
   );
 };
 
-export default FormLogin;
+export default FormSignin;

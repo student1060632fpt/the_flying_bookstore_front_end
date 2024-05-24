e942e972548a192163b267e8b143a4a327b5685d
student1060632fpt <haveaniceday6174@gmail.com>
2024-05-24 10:16:12 +0700

    Refactor OrderInfoRent component to use destructuring for order object fix lỗi null undefined order

--- src/components/checkout/OrderInfoRent.tsx
+++ src/components/checkout/OrderInfoRent.tsx
@@ -8,14 +8,14 @@ import { PiPhone } from "react-icons/pi";
 import { formatPhoneNumber } from "../../utils/helps";
 
 const OrderInfoRent = ({ order }: { order: IOrder | undefined }) => {
-  const cart = useStoreCart((state) => state.cart);
+  const { lessor,leaseOrder } = order || {};
   return (
     <div className="total">
       <div className="total__row">
         <GrUserManager className="total__icon" />
         <p className="total__title">Người cho thuê</p>
         <p className="total__description">
-        {order?.lessor.lastName} {order?.lessor.firstName} 
+        {lessor?.lastName} {lessor?.firstName} 
         </p>
       </div>
 
@@ -23,20 +23,20 @@ const OrderInfoRent = ({ order }: { order: IOrder | undefined }) => {
         <RiCalendarTodoLine className="total__icon" />
         <p className="total__title">Thời gian thuê</p>
         <p className="total__description">
-          {dayjs(order?.leaseOrder.fromDate).format("DD/MM/YYYY")} -{" "}
-          {dayjs(order?.leaseOrder.toDate).format("DD/MM/YYYY")}
+          {dayjs(leaseOrder?.fromDate).format("DD/MM/YYYY")} -{" "}
+          {dayjs(leaseOrder?.toDate).format("DD/MM/YYYY")}
         </p>
       </div>
 
       <div className="total__row">
         <RiMapPin2Line className="total__icon" />
         <p className="total__title">Địa chỉ</p>
-        <p className="total__description">{order?.leaseOrder.lessorAddress}</p>
+        <p className="total__description">{leaseOrder?.lessorAddress}</p>
       </div>
       <div className="total__row">
         <PiPhone className="total__icon" />
         <p className="total__title">Số điện thoại</p>
-        <p className="total__description">{formatPhoneNumber(order?.lessor.phoneNumber)}</p>
+        <p className="total__description">{formatPhoneNumber(lessor?.phoneNumber)}</p>
       </div>
     </div>
   );

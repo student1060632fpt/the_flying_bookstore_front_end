b8c90978a2c7ba46fbe88bae703f12adcb181b96
student1060632fpt <haveaniceday6174@gmail.com>
2024-05-26 20:10:39 +0700

    Refactor manager-post/page.tsx to update component imports and remove unused code

--- src/app/(manager)/manager-post/page.tsx
+++ src/app/(manager)/manager-post/page.tsx
@@ -1,31 +1,47 @@
 "use client";
-import {  useState } from "react";
-import Link from "next/link";
-import { Box, Button, Stack, Typography } from "@mui/material";
-
-const ManagerPost = () => {
-
-
-
+import { Button, Typography } from "@mui/material";
+import CreateBook from "@/components/createPost/CreateBook";
+import CreateDocument, {
+  TFieldDocumentValue,
+} from "@/components/createPost/CreateDocument";
+import CreatePost, {
+  TFieldPostValue,
+} from "@/components/createPost/CreatePost";
+import { FormProvider, SubmitHandler, useForm } from "react-hook-form";
+import { useState } from "react";
+type TFieldValue = TFieldDocumentValue & TFieldPostValue;
+export interface IPostState {
+  bookId: number | string | undefined;
+  copyId: string | undefined;
+  postId: string | undefined;
+}
+const AddPost = () => {
+  const [post, setPost] = useState<IPostState>({
+    bookId: undefined,
+    copyId: undefined,
+    postId: undefined,
+  });
+  const updateBookId = (bookId: IPostState["bookId"]): void =>
+    setPost((state) => ({ ...state, bookId }));
+  const updateDocumentId = (copyId: IPostState["copyId"]): void =>
+    setPost((state) => ({ ...state, copyId }));
+  const methods = useForm<TFieldValue>();
+  const onSubmit: SubmitHandler<TFieldValue> = (data) => console.log(data);
 
   return (
     <>
-      <Stack
-        direction="row"
-        justifyContent="space-between"
-        alignItems="center"
-        sx={{ mb: 2 }}
-      >
-        <Typography variant="h4" gutterBottom>
-          Quản lý bài đăng của tôi
-        </Typography>
-        <Link href="/manager-post/add-post">
-          <Button variant="contained">Thêm bài đăng</Button>
-        </Link>
-      </Stack>
+      <Typography variant="h4" sx={{ mb: 2 }}>
+        Đăng bài mới
+      </Typography>
 
+      <CreateBook updateBookId={updateBookId} />
+      <CreateDocument
+        bookId={post.bookId}
+        updateDocumentId={updateDocumentId}
+      />
+      <CreatePost copyId={post.copyId}/>
     </>
   );
 };
 
-export default ManagerPost;
+export default AddPost;

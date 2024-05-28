6d2e32a2ed7dd9a95d2f2c0a50485c450be17701
student1060632fpt <haveaniceday6174@gmail.com>
2024-05-28 14:19:45 +0700

    fix api auth and experimental: { missingSuspenseWithCSRBailout: false, },

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
  
 module.exports = {
+  experimental: { missingSuspenseWithCSRBailout: false, },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
